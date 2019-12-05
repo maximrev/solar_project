005e50d4c55d880d7d80fadcab3cb3592d0dad7b
reva.ma@phystech.edu <reva.ma@phystech.edu>
2019-12-05 04:48:42 +0300

    1234

--- solar_input.py
+++ solar_input.py
@@ -7,9 +7,7 @@ from solar_objects import Star, Planet
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-
     Параметры:
-
     **input_filename** — имя входного файла
     """
 
@@ -30,38 +28,33 @@ def read_space_objects_data_from_file(input_filename):
 
 
 def parse_star_parameters(line, star):
-    """Считывает данные о звезде из строки.
-    Входная строка должна иметь слеюущий формат:
-    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
-    Здесь (x, y) — координаты зведы, (Vx, Vy) — скорость.
-    Пример строки:
-    Star 10 red 1000 1 2 3 4
 
-    Параметры:
-
-    **line** — строка с описание звезды.
-    **star** — объект звезды.
-    """
-
-    pass  # FIXME: not done yet
+    line = line.lower()
+    line_parsed = line.split()
+    star.type = line_parsed[0]
+    star.R = int(line_parsed[1])
+    star.color = line_parsed[2]
+    star.m = float(line_parsed[3])
+    star.x = float(line_parsed[4])
+    star.y = float(line_parsed[5])
+    star.Vx = float(line_parsed[6])
+    star.Vy = float(line_parsed[7])
+    return
 
 def parse_planet_parameters(line, planet):
-    """Считывает данные о планете из строки.
-    Предполагается такая строка:
-    Входная строка должна иметь слеюущий формат:
-    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
 
-    Здесь (x, y) — координаты планеты, (Vx, Vy) — скорость.
-    Пример строки:
-    Planet 10 red 1000 1 2 3 4
-
-    Параметры:
-
-    **line** — строка с описание планеты.
-    **planet** — объект планеты.
-    """
-    pass  # FIXME: not done yet...
+    line = line.lower()
+    line_parsed = line.split()
+    planet.type = line_parsed[0]
+    planet.R = int(line_parsed[1])
+    planet.color = line_parsed[2]
+    planet.m = float(line_parsed[3])
+    planet.x = float(line_parsed[4])
+    planet.y = float(line_parsed[5])
+    planet.Vx = float(line_parsed[6])
+    planet.Vy = float(line_parsed[7])
+    return
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -69,16 +62,25 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     Строки должны иметь следующий формат:
     Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
     Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
-
     Параметры:
-
     **output_filename** — имя входного файла
     **space_objects** — список объектов планет и звёзд
     """
+
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
+            Fout = open(output_filename, "w")
+            if obj.type == "star":
+                temp = "Star"
+            else:
+                obj.type = "planet"
+                temp = "Planet"
+
+            Fout.write(temp+str(R)+str(color)+str(m)+str(x)+str(y)+str(Vx)+str(Vy))
             print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
             # FIXME: should store real values
+
+    Fout.close()
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
--- solar_model.py
+++ solar_model.py
@@ -7,9 +7,7 @@ gravitational_constant = 6.67408E-11
 
 def calculate_force(body, space_objects):
     """Вычисляет силу, действующую на тело.
-
     Параметры:
-
     **body** — тело, для которого нужно вычислить дейстующую силу.
     **space_objects** — список объектов, которые воздействуют на тело.
     """
@@ -18,30 +16,30 @@ def calculate_force(body, space_objects):
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        f = - gravitational_constant * body.m * obj.m / r ** 2
+        body.Fx += f * (body.x - obj.x) / r
+        body.Fy += f * (body.y - obj.y) / r
 
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
-
     Параметры:
-
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    ax = body.Fx / body.m
+    body.x += body.Vx * dt
+    body.Vx += ax * dt
+
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt
+    body.Vy += ay * dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
-
     Параметры:
-
     **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
     **dt** — шаг по времени
     """
@@ -54,3 +52,8 @@ def recalculate_space_objects_positions(space_objects, dt):
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
+
+
+
+
+
